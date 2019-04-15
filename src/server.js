c6071d186284ce898cca3269af7b556ebf477a44
Saulo de sousa Joseph <saulos.joseph@gmail.com>
2019-04-15 20:29:06 -0300

    Estrutura backend

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const Server = require('http').Server(app);
+const io = require('socket.io')(Server);
+
+io.on('connection', scoket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+})
+
+mongoose.connect('mongodb+srv://estudos:estudos321@primeiro-meh8g.mongodb.net/omnistack?retryWrites=true', {
+    useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(require('./routes'));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+Server.listen(3333);
